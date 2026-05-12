42492bae93330c893d977d15e9ba7c0828ca14c4
johnjoi <147251129+johnjoi@users.noreply.github.com>
2026-05-12 18:27:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const shopifyDomain = "entryleveljobs.onlinejob247.com";
+  const shopifyDomain = "newstore09.sarkarivaccancy.com";
   const proxyHost = req.headers.host;
 
   const targetURL = `https://${shopifyDomain}${req.url}`;
